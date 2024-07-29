e3a1c2d7be963b7d5d8b574ebe09a4439ac20cc1
DanielThatu47 <141019551+DanielThatu47@users.noreply.github.com>
2024-07-29 18:25:12 +0530

    Update NetworkStatus.js

--- src/components/NetworkStatus/NetworkStatus.js
+++ src/components/NetworkStatus/NetworkStatus.js
@@ -1,28 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
+import { useOffline } from '@react-hook/offline';
 import NoInternetImage from './no internet.png'; // Adjust the path as needed
 import './NetworkStatus.css'; // Custom styles for the component
 
 const NetworkStatus = () => {
-  const [isOffline, setIsOffline] = useState(!navigator.onLine);
-
-  useEffect(() => {
-    const handleOnline = () => {
-      setIsOffline(false);
-    };
-
-    const handleOffline = () => {
-      setIsOffline(true);
-    };
-
-    window.addEventListener('online', handleOnline);
-    window.addEventListener('offline', handleOffline);
-
-    // Cleanup listeners on unmount
-    return () => {
-      window.removeEventListener('online', handleOnline);
-      window.removeEventListener('offline', handleOffline);
-    };
-  }, []);
+  const isOffline = useOffline();
 
   if (isOffline) {
     return (
