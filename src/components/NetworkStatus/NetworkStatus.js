e720556258c5a28000986a1188e217531adc3878
DanielThatu47 <141019551+DanielThatu47@users.noreply.github.com>
2024-07-29 18:41:25 +0530

    Update NetworkStatus.js

--- src/components/NetworkStatus/NetworkStatus.js
+++ src/components/NetworkStatus/NetworkStatus.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import { useOffline } from '@react-hook/offline';
+import { Detector } from 'react-detect-offline';
 import NoInternetImage from './no internet.png'; // Adjust the path as needed
 import './NetworkStatus.css'; // Custom styles for the component
 
-const NetworkStatus = () => {
-  const isOffline = useOffline();
-
-  if (isOffline) {
-    return (
-      <div className="network-status">
-        <img src={NoInternetImage} alt="No Internet Connection" />
-        <p>You are currently offline. Please check your internet connection.</p>
-      </div>
-    );
-  }
-
-  return null;
-};
+const NetworkStatus = () => (
+  <Detector
+    render={({ online }) => (
+      !online && (
+        <div className="network-status">
+          <img src={NoInternetImage} alt="No Internet Connection" />
+          <p>You are currently offline. Please check your internet connection.</p>
+        </div>
+      )
+    )}
+  />
+);
 
 export default NetworkStatus;
