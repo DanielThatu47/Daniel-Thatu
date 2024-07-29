1983395e3afbc84648cdeb78d6d46dcc83fd4b8e
DanielThatu47 <141019551+DanielThatu47@users.noreply.github.com>
2024-07-29 18:54:44 +0530

    Update NetworkStatus.js

--- src/components/NetworkStatus/NetworkStatus.js
+++ src/components/NetworkStatus/NetworkStatus.js
@@ -3,10 +3,12 @@ import { Detector } from 'react-detect-offline';
 import NoInternetImage from './no internet.png'; // Adjust the path as needed
 import './NetworkStatus.css'; // Custom styles for the component
 
-const NetworkStatus = () => (
+const NetworkStatus = ({ children }) => (
   <Detector
     render={({ online }) => (
-      !online && (
+      online ? (
+        children
+      ) : (
         <div className="network-status">
           <img src={NoInternetImage} alt="No Internet Connection" />
           <p>You are currently offline. Please check your internet connection.</p>
