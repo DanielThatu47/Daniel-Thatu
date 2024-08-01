87f1e582394782a4cfe3339f07a1bfbe8ae8a648
DanielThatu47 <141019551+DanielThatu47@users.noreply.github.com>
2024-08-01 23:40:55 +0530

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'offline-cache';
+const urlsToCache = [
+  '/',
+  '/demo.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
