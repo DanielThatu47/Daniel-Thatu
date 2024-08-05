a18c035f8ec7057fd7973709c2d7a83808244df9
DanielThatu47 <141019551+DanielThatu47@users.noreply.github.com>
2024-08-05 08:17:33 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,15 +13,14 @@ root.render(
 );
 
 
-// Register the service worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register(`${process.env.PUBLIC_URL}/service-worker.js`)
-      .then(registration => {
-        console.log('Service Worker registered with scope:', registration.scope);
+    navigator.serviceWorker.register('/service-worker.js')
+      .then((registration) => {
+        console.log('ServiceWorker registration successful with scope: ', registration.scope);
       })
-      .catch(error => {
-        console.error('Service Worker registration failed:', error);
+      .catch((error) => {
+        console.log('ServiceWorker registration failed: ', error);
       });
   });
 }
