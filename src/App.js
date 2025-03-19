d7ce6b76e70013230cf1786ea9888110a0b97871
DanielThatu47 <141019551+DanielThatu47@users.noreply.github.com>
2025-03-19 17:42:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,47 +10,47 @@ import './style.css';
 import About from './components/About/About';
 import { Element } from 'react-scroll';
 import NetworkStatus from './components/NetworkStatus/NetworkStatus';
+import ScrollToTop from './components/ScrollToTop';
 
 const App = () => (
+  <>
+    <NetworkStatus>
+      <div>
+        <Navbar />
+        <Element name="about">
+          <div id="about">
+            <About />
+          </div>
+        </Element>
 
-<>
- <NetworkStatus>
-  <div>
-    <Navbar />
-    <Element name="about">
-    <div id="about">
-      <About />
-    </div>
-    </Element>
+        <Element name="Education">
+          <div id="education">
+            <Education />
+          </div>
+        </Element>
 
-    <Element name="Education">
-    <div id="education">
-      <Education />
-    </div>
-    </Element>
+        <Element name="skills">
+          <div id="skills">
+            <Skills />
+          </div>
+        </Element>
+        <div id="projects">
+          <Projects />
+        </div>
+        <div id="achievements">
+          <Achievements />
+        </div>
+        <div id="certifications">
+          <Certifications />
+        </div>
+        <div id="contact">
+          <Footer />
+        </div>
 
-    <Element name="skills">
-    <div id="skills">
-      <Skills />
-    </div>
-    </Element>
-    <div id="projects">
-      <Projects />
-    </div>
-    <div id="achievements">
-      <Achievements />
-    </div>
-    <div id="certifications">
-      <Certifications />
-    </div>
-    <div id="contact">
-      <Footer />
-    </div>
-  
-  </div>
-</NetworkStatus>
-
-</>
+        <ScrollToTop />
+      </div>
+    </NetworkStatus>
+  </>
 );
 
 export default App;
